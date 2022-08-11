3a3fcc6eac8a9657468d6e16532e12134a76a3b9
Collins Igeh <igehac@gmail.com>
2022-08-11 13:32:30 +0100

    implemented a graphql api for clients with sample data

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema.js');
 const port = process.env.PORT || 5000;
 
 app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}));
 
 app.listen(port, () => {
     console.log(`Server running at port: ${port}`);
