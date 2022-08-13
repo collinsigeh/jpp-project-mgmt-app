7054c5bd7f48c589e1d6b7f49266783a054f3c90
Collins Igeh <igehac@gmail.com>
2022-08-13 09:39:31 +0100

    modified the .env file with DEV and PROD MONGO_URIs. Also created the models

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const colors = require('colors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema.js');
+const schema = require('./schema/schema');
 const port = process.env.PORT || 5000;
+const connectDB = require('./config/db');
 
 app = express();
+
+// connect to database
+connectDB();
 
 app.use('/graphql', graphqlHTTP({
     schema,
