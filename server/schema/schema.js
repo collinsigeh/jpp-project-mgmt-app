abcd9a1398523669511e56ac0931741dc39b9cc4
Collins Igeh <igehac@gmail.com>
2022-08-13 11:49:08 +0100

    implemented code to add client

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,14 @@
 const Project = require('../models/Project');
 const Client = require('../models/Client');
 
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList,  } = require('graphql');
+const { 
+    GraphQLObjectType, 
+    GraphQLID, 
+    GraphQLString, 
+    GraphQLSchema, 
+    GraphQLList, 
+    GraphQLNonNull,  
+} = require('graphql');
 
 // Project Type
 const ProjectType = new GraphQLObjectType({
@@ -31,6 +38,8 @@ const ClientType = new GraphQLObjectType({
     })
 });
 
+
+// Queries
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -63,6 +72,31 @@ const RootQuery = new GraphQLObjectType({
     }
 });
 
+// Mutations
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addClient: {
+            type: ClientType,
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                email: { type: GraphQLNonNull(GraphQLString) },
+                phone: { type: GraphQLNonNull(GraphQLString) }
+            },
+            resolve(parent, args) {
+                const client = new Client({
+                    name: args.name,
+                    email: args.email,
+                    phone: args.phone
+                });
+
+                return client.save();
+            }
+        }
+    }
+});
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation
 });
